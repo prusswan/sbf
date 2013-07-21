723c0b515d42c2eeca6c837575ecb1b9a7ebc59a
prusswan <prusswan@gmail.com>
2013-07-21 20:53:29 +0800

    added steps to access units for each block

--- spec/features/brochure_spec.rb
+++ spec/features/brochure_spec.rb
@@ -7,21 +7,56 @@ describe "BrochureSpecs" do
   describe "GET /brochure_specs" do
     it "load SBF main page" do
       visit sbf_link
-      page.find(:xpath, details_dropdown).trigger(:mouseover)
 
       within('div#cssdrivemenu1') do
-        link = find_link('Geylang')
-        link['onclick'].should == "goFlats('../../13MAYSBF_page_5789/$file/map.htm?open&ft=sbf&twn=GL')"
+        while not page.current_url.end_with?('&twn=GL') do
+          while true
+            dropdown = page.all(:xpath, details_dropdown)
 
-        link.click
+            if dropdown.count > 0
+              dropdown.first.trigger(:mouseover)
+              break
+            end
+          end
+
+          link = find_link('Geylang')
+          link['onclick'].should == "goFlats('../../13MAYSBF_page_5789/$file/map.htm?open&ft=sbf&twn=GL')"
+
+          link.click
+        end
       end
-
-      page.current_url.should end_with('&twn=GL')
 
       within_frame 'fda' do
         # puts page.body
-        flat_type = page.all(:xpath, "//select[@name='Flat']/option")
-        flat_type.count.should == 5
+        flat_types = page.all(:xpath, "//select[@name='Flat']/option")
+        flat_types.count.should == 5
+
+        select flat_types[1].text, from: 'select7'
+
+        click_button 'Search'
+        sleep 1
+
+        within_frame 'search' do
+          # block_nos = page.all(:xpath, "//td[descendant::strong]")
+
+          block_nos = page.all(:xpath, "//tr[td[div[font[strong[contains(.,'Click on block no')]]]]]/following-sibling::tr//a")
+
+          links = block_nos.map { |b| [b.text, b[:href]] }
+          puts block_nos.count
+
+          links.each do |link|
+            page.execute_script(link.last)
+
+            page.find(:xpath, "//tr[td[div[font[strong[contains(.,'Click on block no')]]]]]/following-sibling::tr//b")
+            # puts page.body
+
+            page.find(:xpath, "//td[@class='textContentNew' and contains(.,'#{link.first}')]")
+            unit_nos = page.all(:xpath, "//table[tbody[tr[td[contains(.,'Mouseover unit number')]]]]/following-sibling::table//font")
+
+            # puts page.body
+            puts "#{link.first} #{unit_nos.count}"
+          end
+        end
       end
     end
   end
