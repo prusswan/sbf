c8acf222c3e5824e1972a5ada122bb82c6d6353d
prusswan <prusswan@gmail.com>
2013-11-15 16:44:32 +0800

    moved migrations: http://pivotallabs.com/leave-your-migrations-in-your-rails-engines/

--- lib/sbf/engine.rb
+++ lib/sbf/engine.rb
@@ -1,8 +1,17 @@
 module Sbf
   class Engine < ::Rails::Engine
+    # isolate_namespace EngineWithMigrations
 
     config.generators do |g|
       g.test_framework :rspec
+    end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s == root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
     end
   end
 end
